cafc6a4191cf938fdabd039188ad3396f614581a
Tobias Hildebrand <tobus3000@tobotec.ch>
2024-02-24 16:46:42 +0100

    update

--- custom_components/openai_response/sensor.py
+++ custom_components/openai_response/sensor.py
@@ -25,7 +25,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 )
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    
     host = config[CONF_HOST]
     port = config[CONF_PORT]
     api_base = f"http://{host}:{port}/v1"
